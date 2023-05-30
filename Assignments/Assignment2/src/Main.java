70fc79c5b4cbf3c104d8901a9bfa1e08d0126844
kritieeee <119518640+kritieeee@users.noreply.github.com>
2023-05-30 15:59:01 +0530

    Add files via upload

---
+++ Assignments/Assignment2/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int a=1;
+        byte b = 3;
+        short sh= 123;
+        long l= 345768;
+        boolean b1= true;
+        float pi=22/7;
+
+        double d=43.4343434;
+
+        char r='s';
+
+        System.out.println("Value of a is "+ a);
+        System.out.println("Value of pi is approximately "+pi);
+        System.out.println("Double value of d = " + d);
+        System.out.println("Character value of r is "+ r );
+        System.out.println("Byte b is "+ b);
+        System.out.println("Short sh = "+sh);
+        System.out.println("Long l = " +l);
+        System.out.println("boolean b1 is "+b1 );
+
+
+    }
+
+}
